152a794a8ef57ac9df71ee68c9f82d7855b00d66
domagojl <96523928+domagojl@users.noreply.github.com>
2022-03-08 20:33:11 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,127 @@
+class Calculator{
+    constructor(previusOperandTextElement, currentOperandTextElement){
+        this.previusOperandTextElement = previusOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+
+    clear() {
+        this.currentOperand = ''
+        this.previusOperand = ''
+        this.operation = undefined
+    }
+
+    delete(){
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
+    }
+
+    appendNumber(number){
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+
+    chooseOperation(operation) {
+        if(this.currentOperand === '') return
+        if(this.previousOperand !== ''){
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+    }
+    
+    compute(){
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if(isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case '*':
+                computation = prev * current
+                break
+            case '÷':
+                computation = prev / current
+                break
+            default:
+                return
+        }
+
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
+    }
+    getDisplayNumber (number) {
+        const stringNumber = number.toString()
+        const intigerDigit = parseFloat(stringNumber.split('.')[0])
+        const decimalDigit = stringNumber.split('.')[1]
+        let intigerDisplay
+        if(isNaN(intigerDigit)){
+            intigerDisplay = ''
+        } else {
+            intigerDisplay = intigerDigit.toLocaleString('en', {maximumFractionDigits: 0})
+        }
+
+        if(decimalDigit != null){
+            return `${intigerDisplay}.${decimalDigit}`
+        } else{
+            return intigerDisplay
+        }
+    }
+
+    updateDisplay(){
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+        if(this.operation != null){
+            this.previusOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else{
+            this.previusOperandTextElement.innerText = ''
+        }
+        
+    }
+}
+
+
+
+const numberButtons = document.querySelectorAll('[data-number')
+const operationButtons = document.querySelectorAll('[data-operation')
+const equalsButtons = document.querySelector('[data-equals')
+const deleteButtons = document.querySelector('[data-delete')
+const allClearButtons = document.querySelector('[data-all-clear')
+const previusOperandTextElement = document.querySelector('[data-previus-operand')
+const currentOperandTextElement  = document.querySelector('[data-current-operand')
+
+const calculator = new Calculator(previusOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click' ,() => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click' ,() => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalsButtons.addEventListener('click' , button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButtons.addEventListener('click' , button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButtons.addEventListener('click' , button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
